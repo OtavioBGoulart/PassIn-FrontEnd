b547855f020aca8bf0704631c94fe1d33c19113c
Otavio Goulart <otaviobgoulartt@gmail.com>
2024-04-02 22:42:15 -0300

    feat: header css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
+import logo from "../assets/logo.svg"
+
 export function Header() {
     return (
-        <h1>
-            Header
-        </h1>
-    )
+        <div className="flex items-center gap-5 py-2">
+            <img src={logo}/>
+
+            <nav className="flex items-center gap-5">
+                <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
+                <a href="" className="font-medium text-sm">Participantes</a>
+            </nav>
+        </div>
+    )        
 }
