7ce15a4a1a6bfd2fe46a0b46591d566ced9f464e
Otavio Goulart <otaviobgoulartt@gmail.com>
2024-04-08 22:30:45 -0300

    feat: table

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,10 +1,56 @@
+import { Search } from "lucide-react";
+
 export function AttendeeList() {
-    return (
-        <div className="flex gap-3 items-center">
-            <h1 className="text-2xl font-bold">Partcipantes</h1>
-            <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center">
-                <input className="bg-transparent flex-1 outline-none" placeholder="Buscar participante..."/>
-            </div>
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-3 items-center">
+        <h1 className="text-2xl font-bold">Partcipantes</h1>
+        <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+          <Search className="size-4 text-emerald-300" />
+          <input
+            className="bg-transparent flex-1 outline-none"
+            placeholder="Buscar participante..."
+          />
         </div>
-    )
-} 
+      </div>
+
+      <div className="border border-white/10 rounded-lg">
+        <table className="w-full">
+          <thead>
+            <tr className="border-b ">
+              <th className="py-3 px-2.5 text-sm font-semibold text-left ">
+                <input type="checkbox" />
+              </th>
+              <th className="py-3 px-2.5 text-sm font-semibold text-left">Código</th>
+              <th className="py-3 px-2.5 text-sm font-semibold text-left">Participante</th>
+              <th className="py-3 px-2.5 text-sm font-semibold text-left">Data de inscrição</th>
+              <th className="py-3 px-2.5 text-sm font-semibold text-left">Data de check in</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-b border-white/10">
+              <td className="py-3 px-2.5 text-sm text-zinc-300">
+                <input type="checkbox" />
+              </td>
+              <td className="py-3 px-2.5 text-sm text-zinc-300">12383</td>
+              <td className="py-3 px-2.5 text-sm text-zinc-300">
+                <div className="flex flex-col gap-1">
+                <span className="font-semibold text-white">Otávio Goulart</span>
+                <span>otavio@gmail.com</span>
+                </div>
+              </td>
+              <td className="py-3 px-2.5 text-sm text-zinc-300">7 dias atrás</td>
+              <td className="py-3 px-2.5 text-sm text-zinc-300">3 dias atrás</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td className="py-3 px-2.5 text-sm text-zinc-300" colSpan={3}>Mostrando 10 de 28 items</td>
+              <td className="py-3 px-2.5 text-sm text-zinc-300" colSpan={3}>Mostrando 10 de 28 items</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
+}
