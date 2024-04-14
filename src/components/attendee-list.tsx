811c6f5573045a2cb82f81504cb73aeff8ae6dea
Otavio Goulart <otaviobgoulartt@gmail.com>
2024-04-13 21:41:55 -0300

    feat: Serach filter

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -4,7 +4,7 @@ import {
   ChevronLeft,
   ChevronRight,
   ChevronsLeft,
-  ChevronsRight
+  ChevronsRight,
 } from "lucide-react";
 import { IconButton } from "./icon-button";
 import { Table } from "./table/table";
@@ -26,7 +26,17 @@ interface Attendee {
 }
 
 export function AttendeeList() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has("search")) {
+      return url.searchParams.get("search") ?? "";
+    }
+
+    return "";
+  });
+
+
   const [page, setPage] = useState(() => {
     const url = new URL(window.location.toString());
 
@@ -35,40 +45,55 @@ export function AttendeeList() {
     }
 
     return 1;
-  })
+  });
 
   const [attendees, setAttendees] = useState<Attendee[]>([]);
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
 
-  const totalPages = Math.ceil(total / 10)
+  const totalPages = Math.ceil(total / 10);
 
   useEffect(() => {
-    const url = new URL("http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees")
+    const url = new URL(
+      "http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees"
+    );
+            
     url.searchParams.set("pageIndex", String(page - 1));
 
-    fetch(
-      url
-    )
+    if (search.length > 1) {
+      url.searchParams.set("query", search);
+    }
+
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setAttendees(data.attendees);
         setTotal(data.total);
       });
-  },[page]);
+  }, [page, search]);
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString());
+
+    url.searchParams.set("search", search);
+
+    window.history.pushState({}, "", url);
+
+    setSearch(search);
+  }
 
   function setCurrentPage(page: number) {
     const url = new URL(window.location.toString());
 
     url.searchParams.set("page", String(page));
 
-    window.history.pushState({}, "", url)
-    setPage(page)
+    window.history.pushState({}, "", url);
+    setPage(page);
   }
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value);
+    setCurrentSearch(event.target.value);
+    setCurrentPage(1);
   }
-
 
   function goToFirstPage() {
     setCurrentPage(1);
@@ -141,9 +166,11 @@ export function AttendeeList() {
                 </TableCell>
                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
                 <TableCell>
-                  {attendee.checkedInAt === null
-                    ? <span className="text-zinc-500">Não fez check-in</span>
-                    : dayjs().to(attendee.checkedInAt)}
+                  {attendee.checkedInAt === null ? (
+                    <span className="text-zinc-500">Não fez check-in</span>
+                  ) : (
+                    dayjs().to(attendee.checkedInAt)
+                  )}
                 </TableCell>
                 <TableCell>
                   <IconButton transparent={true}>
@@ -157,10 +184,14 @@ export function AttendeeList() {
 
         <tfoot>
           <tr>
-            <TableCell colSpan={3}>Mostrando {attendees.length} de {total} items</TableCell>
+            <TableCell colSpan={3}>
+              Mostrando {attendees.length} de {total} items
+            </TableCell>
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
-                <span>Página {page} de {totalPages}</span>
+                <span>
+                  Página {page} de {totalPages}
+                </span>
                 <div className="flex gap-1.5">
                   <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
@@ -168,10 +199,16 @@ export function AttendeeList() {
                   <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton onClick={goToNextPage} disabled={page === totalPages}>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton onClick={goToLastPage} disabled={page === totalPages}>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
